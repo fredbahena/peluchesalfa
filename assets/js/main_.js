25acc1255d56bbd8d5794a13372567df56203948
Fred <fred.bahena@gmail.com>
2021-07-16 03:23:19 -0500

    Versión primera

--- assets/js/main_.js
+++ assets/js/main_.js
@@ -1,5 +1,5 @@
 let show_copy = () => {
     var year = new Date().getFullYear()
-    var htmlString = "&copy; " + year + " Copyright <strong><span>Magic Digital Marketing</span></strong>. Todos los derechos reservados.";
+    var htmlString = "&copy; " + year + " Copyright <strong><span>Peluches Alfa</span></strong>. Todos los derechos reservados.";
         $("#copy").html( htmlString );
 }
